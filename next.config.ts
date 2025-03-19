5bcbc742f99a5ec1e56e64b875fce9ae43dda17d
Patricio Gonzalez <patote.gonzalez@gmail.com>
2025-03-19 05:08:00 -0300

    install shadcn and move to react 18

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["http://localhost", "http://127.0.0.1", "http://127.0.0.1:62981"],
 };
 
 export default nextConfig;
