399af28e7ef82211185157fa5edd130c1dc69625
Patricio Gonzalez <patote.gonzalez@gmail.com>
2025-03-19 05:28:02 -0300

    fix stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <div>Hot 222 Test with standard Next.js server: {new Date().toLocaleTimeString()}</div>
+   <div>Hot 222 Test with standard Next.js server</div>
   );
 }
